e071e2b7aaa846df98a13326affe333136bd86aa
Florian <flo@sitecore.com>
2024-01-20 19:11:32 +0100

    Correct UVs

--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -1,8 +1,9 @@
-import { MeshProps } from "@react-three/fiber";
-import { useControls } from "leva";
-import { FunctionComponent } from "react";
 import useTexture from "./useTexture";
-import { RepeatWrapping, Texture } from "three";
+import { useControls } from "leva";
+import { MeshProps } from "@react-three/fiber";
+import { FunctionComponent, useMemo } from "react";
+import { BoxGeometry, RepeatWrapping, Texture } from "three";
+import { MeshReflectorMaterial } from "@react-three/drei";
 
 interface ShelfProps extends MeshProps {
     length: number;
@@ -14,22 +15,56 @@ const Shelf: FunctionComponent<ShelfProps> = ({ length, thickness, depth, ...mes
     const { wireframe, texture } = useControls("properties", {
         wireframe: false,
         texture: {
-            value: "marble_01",
             options: ["marble_01", "plywood", "dark_wood"]
         }
     })
 
     const textures = useTexture(texture, (textures) => {
-        for (const texture of (textures as Texture[])) {
-            texture.wrapS = RepeatWrapping
-            texture.wrapT = RepeatWrapping
-            texture.repeat.set(length, depth)
-            texture.needsUpdate = true;
-        }
+        (textures as Texture[]).forEach(texture => {
+            texture.wrapS = texture.wrapT = RepeatWrapping
+        })
     });
 
+    const geometry = useMemo(() => {
+        const geometry = new BoxGeometry(length, thickness, depth)
+        const uv = geometry.getAttribute("uv")
+
+        for (let i = 0; i < uv.count; i++) {
+            const x = uv.getX(i);
+            const y = uv.getY(i);
+            const third = uv.count / 3;
+
+            // Left and right side
+            if (i < third) {
+                uv.setXY(
+                    i,
+                    x * thickness,
+                    y * depth
+                )
+            }
+            // Top and bottom
+            else if (i < 2 * third) {
+                uv.setXY(
+                    i,
+                    x * length,
+                    y * depth
+                )
+            }
+            // Front and back
+            else {
+                uv.setXY(
+                    i,
+                    x * length,
+                    y * thickness
+                )
+            }
+        }
+
+        return geometry
+    }, [length, thickness, depth])
+
     return <mesh {...meshProps}>
-        <boxGeometry args={[length, thickness, depth]} />
+        <primitive object={geometry} attach="geometry" />
         <meshStandardMaterial {...textures} wireframe={wireframe} />
     </mesh>
 }
